c12e8a0916c2ff25e9153afd388dec0e6d9a4b2d
Ben Faingold <benfaingold@gmail.com>
2016-12-12 15:10:53 +0200

    Bugfix TriviaActivity.java

--- app/src/main/java/com/example/mr_starktastic/ex3/TriviaActivity.java
+++ app/src/main/java/com/example/mr_starktastic/ex3/TriviaActivity.java
@@ -72,7 +72,7 @@ public class TriviaActivity extends AppCompatActivity
         switch (view.getId()) {
             case R.id.previous_button:
                 int index = questionPager.getCurrentItem() - 1;
-                questionPager.setCurrentItem(index > 0 ? index : questionAdapter.getCount() - 1);
+                questionPager.setCurrentItem(index >= 0 ? index : questionAdapter.getCount() - 1);
                 break;
 
             case R.id.next_button:
